d73870895e44cb8b2c45adda293d5fa41d29f1e9
Karl Ryden <karlrydenn@gmail.com>
2021-02-05 11:56:30 +0100

    :)

--- plot.py
+++ plot.py
@@ -1,16 +1,27 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 months = ['April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September']
 regions = ['Norrbotten', 'Västra Götaland', 'Dalarna']
 
-data = pd.read_csv('./data/csv/Norrbotten/Juli.csv')
+data = pd.read_csv('./data/csv/Norrbotten.csv', index_col=0)
 
-T = data['År▼'].to_numpy()
-B = data['Areal\nProduktiv skogsmark (m²)'] + data['  \nAnnan trädbevuxen mark (m²)'].to_numpy()
+# print(data)
 
-print(data.head())
-print(data.columns)
+T = [t for t in range(22*6)]
+D = []
 
-plt.plot(T, B)
+for i in range(22):
+    D.append(data.iloc[i].to_numpy())
+
+D = np.concatenate(D)
+print(D)
+
+plt.plot(T, D)
 plt.show()
+
+# for i in range(22):
+#     plt.plot(data.iloc[i])
+#     plt.pause(0.1)
+# plt.show()
