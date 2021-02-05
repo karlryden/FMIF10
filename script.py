12ca951cb2c196cb27184183d3ac31f455330bab
Karl Ryden <karlrydenn@gmail.com>
2021-02-05 11:33:09 +0100

    data

--- script.py
+++ script.py
@@ -1,17 +1,24 @@
 import pandas as pd 
+import numpy as np
 
+years = [year for year in range(1998, 2020)]
 months = ['April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September']
 regions = ['Norrbotten', 'Västra Götaland', 'Dalarna']
 
 for region in regions:
+    series = [pd.Series(years)]
     for month in months:
         target = './data/xlsx/' + region + '/' + month + '.xlsx'
-        path = './data/csv/' + region + '/' + 'test' + '.csv'
-        excel = pd.read_excel(target, 'Data', dtype=str).fillna(0)
-        print(excel.iloc[3])
-        # s = excel.iloc[3] + excel.iloc[4]
-        # print(s)
-        break
-        # data.to_csv(path, encoding='utf-8', index=False)
-        
-    break
+        path = './data/csv/' + region + '.csv'
+        excel = pd.read_excel(target, 'Data', usecols=[0, 3, 4], dtype=float).fillna(0)
+        series.append((excel.iloc[:,1] + excel.iloc[:,2]).astype(int))
+    data = pd.concat(series, axis=1).head()
+    labels = ['År']
+    for m in months:
+        labels.append(m)
+
+    data.columns = labels
+    print(data.head())
+    data.set_index('År', inplace=True)
+    print(data.head())
+    data.to_csv(path)
